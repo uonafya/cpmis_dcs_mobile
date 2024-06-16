81ce0ecf83e012545402802ceb13b3c1de0ed2cd
reubendeekay <mrreubenyt@gmail.com>
2024-06-16 15:27:03 +0300

    hotfix/update splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:cpims_dcs_mobile/controller/auth_provider.dart';
+import 'package:cpims_dcs_mobile/controller/connection_provider.dart';
 import 'package:cpims_dcs_mobile/core/constants/theme.dart';
 import 'package:cpims_dcs_mobile/core/network/http_client.dart';
 import 'package:cpims_dcs_mobile/core/network/preferences.dart';
 import 'package:cpims_dcs_mobile/views/screens/auth/login_screen.dart';
+import 'package:cpims_dcs_mobile/views/screens/auth/splash_screen.dart';
+import 'package:cpims_dcs_mobile/views/widgets/initial_loader.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,10 +23,64 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(create: (_) => ConnectivityProvider()),
+      ChangeNotifierProvider(create: (_) => AuthProvider()),
+    ], child: const CPIMS());
+  }
+}
+
+class CPIMS extends StatefulWidget {
+  const CPIMS({super.key});
+
+  @override
+  State<CPIMS> createState() => _CPIMSState();
+}
+
+class _CPIMSState extends State<CPIMS> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'CPIMS DCS Mobile',
+      title: 'CPIMS',
+      debugShowCheckedModeBanner: false,
       theme: appTheme(),
-      home: const LoginScreen(),
+      home: Builder(
+        builder: (context) {
+          return FutureBuilder(
+            future: intialSetup(context),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const SplashScreen();
+              }
+
+              return snapshot.data!['hasConnection'] == false ||
+                      snapshot.data!['isAuthenticated']
+                  ? const InitialLoaderScreen()
+                  : const LoginScreen();
+            },
+          );
+        },
+      ),
     );
   }
+}
+
+Future<Map<String, dynamic>> intialSetup(BuildContext context) async {
+  final hasConnection =
+      await Provider.of<ConnectivityProvider>(context, listen: false)
+          .checkInternetConnection();
+  if (hasConnection == false) {
+    return {'hasConnection': hasConnection, 'isAuthenticated': false};
+  }
+  final isAuthenticated =
+      // ignore: use_build_context_synchronously
+      await Provider.of<AuthProvider>(context, listen: false)
+          .verifyToken(context: context);
+
+  return {'hasConnection': hasConnection, 'isAuthenticated': isAuthenticated};
 }
