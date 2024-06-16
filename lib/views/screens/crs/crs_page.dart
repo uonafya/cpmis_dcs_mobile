c7e622f908332a034f6475c8218879f6616e2d66
Roman Njoroge <roman.njoroge@njuguna.com>
2024-06-16 17:21:26 +0300

    feat: Created stateful widget

---
+++ lib/views/screens/crs/crs_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CaseRegistrationSheet extends StatefulWidget {
+  const CaseRegistrationSheet({super.key});
+
+  @override
+  State<CaseRegistrationSheet> createState() =>
+      _CaseRegistrationSheetState();
+}
+
+class _CaseRegistrationSheetState extends State<CaseRegistrationSheet> {
+  var selectedState = 0;
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
