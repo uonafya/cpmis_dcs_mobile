797bece6009bd3c616e1701843f53c63b7824ec9
MainaKarl <mainakarlg@gmail.com>
2024-06-17 11:40:18 +0300

    Form fields updated

--- lib/views/screens/crs/register_new_child.dart
+++ lib/views/screens/crs/register_new_child.dart
@@ -79,7 +79,6 @@ class _RegisterNewChildScreenState extends State<RegisterNewChildScreen> {
                   });
                 },
               ),
-              // Checkbox(value: false, onChanged: (val) {}),
               const SizedBox(height: 10),
               const Text(
                 'Provides services directly to children *',
