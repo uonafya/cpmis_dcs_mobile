adb9e18f4e081f85310f0c6d8396f07b843052d2
Roman Njoroge <roman.njoroge@njuguna.com>
2024-06-17 12:41:40 +0300

    feat: Middle of case data

--- lib/views/screens/crs/widgets/form_page_heading.dart
+++ lib/views/screens/crs/widgets/form_page_heading.dart
@@ -2,23 +2,42 @@ import 'package:flutter/material.dart';
 
 class FormPageHeading extends StatelessWidget {
   final String heading;
+  final bool hasClosePage;
 
-  const FormPageHeading({required this.heading, super.key});
+  const FormPageHeading(
+      {required this.heading, this.hasClosePage = false, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            heading,
-            style: const TextStyle(fontSize: 21),
-          ),
-        ),
+        hasClosePage == true
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  pageHeading(heading),
+                  IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.cancel,
+                        color: Colors.grey,
+                      ))
+                ],
+              )
+            : Align(
+                alignment: Alignment.centerLeft, child: pageHeading(heading)),
         const Divider()
       ],
     );
   }
+}
+
+Text pageHeading(String title) {
+  return Text(
+    title,
+    style: const TextStyle(fontSize: 21),
+  );
 }
