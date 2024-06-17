ddff6720d36d616361ea860c7972e68575418dc8
reubendeekay <mrreubenyt@gmail.com>
2024-06-17 15:38:27 +0300

    Link social inquiry form

--- lib/views/screens/homepage/custom_drawer.dart
+++ lib/views/screens/homepage/custom_drawer.dart
@@ -1,9 +1,11 @@
+import 'package:cpims_dcs_mobile/controller/auth_provider.dart';
 import 'package:cpims_dcs_mobile/views/screens/crs/crs_home.dart';
 import 'package:cpims_dcs_mobile/views/screens/follow_up/follow_up_home.dart';
 import 'package:cpims_dcs_mobile/views/screens/homepage/home_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:provider/provider.dart';
 
 class CustomDrawer extends StatefulWidget {
   const CustomDrawer({super.key});
@@ -55,50 +57,71 @@ class _CustomDrawerState extends State<CustomDrawer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
+      body: Column(
         children: [
-          const SizedBox(height: 10),
-          Row(
-            children: [
-              const SizedBox(
-                width: 14,
-              ),
-              Image.asset(
-                'assets/images/logo_gok.png',
-                height: 30,
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              const Text("CPIMS-DCS",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black)),
-            ],
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          const Divider(),
-          ...drawerItems.map(
-            (item) => InkWell(
-              onTap: item['onTap'],
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 14, vertical: 9),
-                    child: Text(
-                      item['title'],
-                      style: const TextStyle(fontSize: 16),
+          Expanded(
+            child: ListView(
+              children: [
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    const SizedBox(
+                      width: 14,
+                    ),
+                    Image.asset(
+                      'assets/images/logo_gok.png',
+                      height: 30,
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Text("CPIMS-DCS",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black)),
+                  ],
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                const Divider(),
+                ...drawerItems.map(
+                  (item) => InkWell(
+                    onTap: item['onTap'],
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 14, vertical: 9),
+                          child: Text(
+                            item['title'],
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                        ),
+                        const Divider()
+                      ],
                     ),
                   ),
-                  const Divider()
-                ],
-              ),
+                ),
+              ],
             ),
+          ),
+          const Divider(
+            height: 1,
+          ),
+          ListTile(
+            title: const Text(
+              "Log out",
+              style: TextStyle(color: Colors.red, fontWeight: FontWeight.w500),
+            ),
+            onTap: () {
+              Provider.of<AuthProvider>(context, listen: false).logOut(context);
+            },
+          ),
+          const SizedBox(
+            height: 4,
           ),
         ],
       ),
