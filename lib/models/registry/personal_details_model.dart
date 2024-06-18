5550ce52dd3e12dbfb43b4e8cbabbecf1817b91b
MainaKarl <mainakarlg@gmail.com>
2024-06-18 14:42:36 +0300

    Personal Details form model added

---
+++ lib/models/registry/personal_details_model.dart
@@ -0,0 +1,57 @@
+class RegisterNewChildModel {
+  String personType;
+  bool isCaregiver;
+  bool providesServicesToChildren;
+  String firstName;
+  String surname;
+  String? otherNames;
+  String sex;
+  String dateOfBirth;
+  String workforceIdName;
+  String datePaperFormFilled;
+
+  RegisterNewChildModel({
+    required this.personType,
+    required this.isCaregiver,
+    required this.providesServicesToChildren,
+    required this.firstName,
+    required this.surname,
+    this.otherNames,
+    required this.sex,
+    required this.dateOfBirth,
+    required this.workforceIdName,
+    required this.datePaperFormFilled,
+  });
+
+  // Converts a RegisterNewChildModel instance to a map for JSON encoding
+  Map<String, dynamic> toJson() {
+    return {
+      'personType': personType,
+      'isCaregiver': isCaregiver,
+      'providesServicesToChildren': providesServicesToChildren,
+      'firstName': firstName,
+      'surname': surname,
+      'otherNames': otherNames,
+      'sex': sex,
+      'dateOfBirth': dateOfBirth,
+      'workforceIdName': workforceIdName,
+      'datePaperFormFilled': datePaperFormFilled,
+    };
+  }
+
+  // Creates a RegisterNewChildModel instance from a map (e.g., decoded JSON)
+  factory RegisterNewChildModel.fromJson(Map<String, dynamic> json) {
+    return RegisterNewChildModel(
+      personType: json['personType'],
+      isCaregiver: json['isCaregiver'],
+      providesServicesToChildren: json['providesServicesToChildren'],
+      firstName: json['firstName'],
+      surname: json['surname'],
+      otherNames: json['otherNames'],
+      sex: json['sex'],
+      dateOfBirth: json['dateOfBirth'],
+      workforceIdName: json['workforceIdName'],
+      datePaperFormFilled: json['datePaperFormFilled'],
+    );
+  }
+}
