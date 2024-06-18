17f066bc69162e5c6f2392724034b5db5506221f
Cliff-Ezra <cliffezra212@gmail.com>
2024-06-18 19:08:40 +0300

    `feat:` create closure follow up model

---
+++ lib/models/closure_followup_model.dart
@@ -0,0 +1,63 @@
+class ClosureFollowupModel {
+  String? caseId;
+  String? caseOutcome;
+  String? transferedTo;
+  List<InterventionList>? interventionList;
+  String? caseClosureNotes;
+  String? dateOfCaseClosure;
+
+  ClosureFollowupModel(
+      {this.caseId,
+      this.caseOutcome,
+      this.transferedTo,
+      this.interventionList,
+      this.caseClosureNotes,
+      this.dateOfCaseClosure});
+
+  ClosureFollowupModel.fromJson(Map<String, dynamic> json) {
+    caseId = json['case_id'];
+    caseOutcome = json['case_outcome'];
+    transferedTo = json['transfered_to'];
+    if (json['intervention_list'] != null) {
+      interventionList = <InterventionList>[];
+      json['intervention_list'].forEach((v) {
+        interventionList!.add(new InterventionList.fromJson(v));
+      });
+    }
+    caseClosureNotes = json['case_closure_notes'];
+    dateOfCaseClosure = json['date_of_case_closure'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['case_id'] = this.caseId;
+    data['case_outcome'] = this.caseOutcome;
+    data['transfered_to'] = this.transferedTo;
+    if (this.interventionList != null) {
+      data['intervention_list'] =
+          this.interventionList!.map((v) => v.toJson()).toList();
+    }
+    data['case_closure_notes'] = this.caseClosureNotes;
+    data['date_of_case_closure'] = this.dateOfCaseClosure;
+    return data;
+  }
+}
+
+class InterventionList {
+  String? intervention;
+  String? caseCategory;
+
+  InterventionList({this.intervention, this.caseCategory});
+
+  InterventionList.fromJson(Map<String, dynamic> json) {
+    intervention = json['intervention'];
+    caseCategory = json['case_category'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['intervention'] = this.intervention;
+    data['case_category'] = this.caseCategory;
+    return data;
+  }
+}
