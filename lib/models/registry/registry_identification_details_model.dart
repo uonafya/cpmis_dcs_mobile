0be673c43eaa3073017c4fc9368e56ae19287866
MainaKarl <mainakarlg@gmail.com>
2024-06-18 12:50:21 +0300

    Registry Identification subform model added

---
+++ lib/models/registry/registry_identification_details_model.dart
@@ -0,0 +1,37 @@
+class RegistryIdentificationModel {
+  String birthRegistrationNumber;
+  String givenName;
+  String countryOfOrigin;
+  String tribe;
+  String religion;
+
+  RegistryIdentificationModel({
+    required this.birthRegistrationNumber,
+    required this.givenName,
+    required this.countryOfOrigin,
+    required this.tribe,
+    required this.religion,
+  });
+
+  // Converts a RegistryIdentificationModel instance to a map for JSON encoding
+  Map<String, String> toJson() {
+    return {
+      'birthRegistrationNumber': birthRegistrationNumber,
+      'givenName': givenName,
+      'countryOfOrigin': countryOfOrigin,
+      'tribe': tribe,
+      'religion': religion,
+    };
+  }
+
+  // Creates a RegistryIdentificationModel instance from a map (e.g., decoded JSON)
+  factory RegistryIdentificationModel.fromJson(Map<String, String> json) {
+    return RegistryIdentificationModel(
+      birthRegistrationNumber: json['birthRegistrationNumber'] ?? "",
+      givenName: json['givenName'] ?? "",
+      countryOfOrigin: json['countryOfOrigin']  ?? "",
+      tribe: json['tribe'] ?? "",
+      religion: json['religion'] ?? "",
+    );
+  }
+}
