49ed509bc22ec5c56889887f081ad12e26037d63
Ted Mwai <teddymwai11@gmail.com>
2024-06-18 12:52:57 +0300

    feat: Update registry caregiver and contact details models

--- lib/models/registry/registry_caregiver_model.dart
+++ lib/models/registry/registry_caregiver_model.dart
@@ -34,24 +34,22 @@ class RegistryCaregiverModel {
       'relationshipToChild': relationshipToChild,
       'nationalIdNumber': nationalIdNumber,
       'phoneNumber': phoneNumber,
-      'isRegistered': isRegistered ? 'true' : 'false',
+      'isRegistered': isRegistered ? true : false,
     };
   }
 
-  factory RegistryCaregiverModel.fromJson(Map<String, String> json) {
+  factory RegistryCaregiverModel.fromJson(Map<String, dynamic> json) {
     return RegistryCaregiverModel(
-      id: json['id'] ?? "",
-      firstName: json['firstName'] ?? "",
-      surName: json['surName'] ?? "",
-      otherNames: json['otherNames'] ?? "",
-      dateOfBirth: json['dateOfBirth'] != null
-          ? DateTime.parse(json['dateOfBirth']!)
-          : DateTime.now(),
-      sex: json['sex'] ?? "",
-      relationshipToChild: json['relationshipToChild'] ?? "",
-      nationalIdNumber: json['nationalIdNumber'] ?? "",
-      phoneNumber: json['phoneNumber'] ?? "",
-      isRegistered: json['isRegistered'] == 'true',
+      id: json['id'],
+      firstName: json['firstName'],
+      surName: json['surName'],
+      otherNames: json['otherNames'],
+      dateOfBirth: json['dateOfBirth'],
+      sex: json['sex'],
+      relationshipToChild: json['relationshipToChild'],
+      nationalIdNumber: json['nationalIdNumber'],
+      phoneNumber: json['phoneNumber'],
+      isRegistered: json['isRegistered'] == true,
     );
   }
 }
