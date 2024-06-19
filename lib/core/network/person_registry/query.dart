bb00eb2eabcaad9dfdf37ce9aa8bb0af8ce04ab1
Ted Mwai <teddymwai11@gmail.com>
2024-06-19 16:23:56 +0300

    feat: Refactor person registry query and database initialization

--- lib/core/network/person_registry/query.dart
+++ lib/core/network/person_registry/query.dart
@@ -1,16 +1,36 @@
 import 'package:cpims_dcs_mobile/core/constants/constants.dart';
 import 'package:cpims_dcs_mobile/core/network/database.dart';
-import 'package:cpims_dcs_mobile/models/registry/personal_details_model.dart';
+import 'package:cpims_dcs_mobile/models/registry/registry_identification_details_model.dart';
 
 class PersonRegistryQuery {
-  Future<void> insertPersonIdentification(RegisterNewChildModel person) async {
+  // insert queries
+  Future<void> insertPersonIdentification(
+      RegistryIdentificationModel person) async {
     // Insert the person's identification details into the database
     try {
       final db = await LocalDB.instance.database;
-      await db.insert(registryIdentificationTable,
-          person.registryIdentificationModel.toJson());
+      await db.insert(registryIdentificationTable, person.toJson());
     } catch (e) {
       print("Error inserting person identification: $e");
+    }
+  }
+
+  // select queries
+  Future<RegistryIdentificationModel> getPersonIdentification() async {
+    // Get the person's identification details from the database
+    try {
+      final db = await LocalDB.instance.database;
+      final List<Map<String, dynamic>> maps =
+          await db.query(registryIdentificationTable);
+      return RegistryIdentificationModel.fromJson(maps.first);
+    } catch (e) {
+      print("Error getting person identification: $e");
+      return RegistryIdentificationModel(
+          birthRegistrationNumber: "",
+          givenName: "",
+          countryOfOrigin: "",
+          tribe: "",
+          religion: "");
     }
   }
 }
