cebd22faa3136d4725a841478c40b0d270525693
Sam Rasugu <mokuasamr@gmail.com>
2024-06-22 12:25:08 +0300

    feat: display caseload events

--- lib/views/screens/follow_up/follow_up_screen.dart
+++ lib/views/screens/follow_up/follow_up_screen.dart
@@ -16,26 +16,6 @@ class FollowUpScreen extends StatefulWidget {
 }
 
 class _FollowUpScreenState extends State<FollowUpScreen> {
-  List<Map<String, dynamic>> caseEvents = [
-    {
-      "eventType": "CLOSURE",
-      "caseCategory": "ALL CASES",
-      "dateOfEvent": "2021-10-10",
-      "details": "First Summon"
-    },
-    {
-      "eventType": "SUMMON",
-      "caseCategory": "ALL CASES",
-      "dateOfEvent": "2021-10-10",
-      "details": ""
-    },
-    {
-      "eventType": "REFFERAL",
-      "caseCategory": "Neglect",
-      "dateOfEvent": "2021-10-10",
-      "details": "Child care program"
-    },
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,20 +33,24 @@ class _FollowUpScreenState extends State<FollowUpScreen> {
               child: ListView(
                 children: [
                   const SizedBox(height: 14),
-                  const Text("CASE SUMMARY",
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black)),
+                  const Text(
+                    "CASE SUMMARY",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
                   Container(
                     padding: const EdgeInsets.all(10),
                     margin: const EdgeInsets.symmetric(vertical: 10),
                     width: double.infinity,
                     decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.grey[300]!,
-                        ),
-                        borderRadius: BorderRadius.circular(4)),
+                      border: Border.all(
+                        color: Colors.grey[300]!,
+                      ),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -80,7 +64,9 @@ class _FollowUpScreenState extends State<FollowUpScreen> {
                             Text(
                               widget.caseLoadModel.caseSerial ?? "-",
                               style: const TextStyle(
-                                  fontSize: 12, color: Colors.grey),
+                                fontSize: 12,
+                                color: Colors.grey,
+                              ),
                             ),
                           ],
                         ),
@@ -118,16 +104,18 @@ class _FollowUpScreenState extends State<FollowUpScreen> {
                     ),
                   ),
                   CustomCard(
-                      title: "Case Events List",
-                      children: List.generate(
-                          caseEvents.length,
-                          (index) => CaseEventItem(
-                                data: caseEvents[index],
-                                onDelete: () {
-                                  caseEvents.removeAt(index);
-                                  setState(() {});
-                                },
-                              ))),
+                    title: "Case Events List",
+                    children: List.generate(
+                      widget.caseLoadModel.events!.length,
+                      (index) => CaseEventItem(
+                        data: widget.caseLoadModel.events![index],
+                        onDelete: () {
+                          widget.caseLoadModel.events!.removeAt(index);
+                          setState(() {});
+                        },
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -138,12 +126,13 @@ class _FollowUpScreenState extends State<FollowUpScreen> {
               text: "Case actions",
               onTap: () {
                 showModalBottomSheet(
-                    context: context,
-                    builder: (context) => SingleChildScrollView(
-                          child: FollowupActions(
-                            caseLoad: widget.caseLoadModel,
-                          ),
-                        ));
+                  context: context,
+                  builder: (context) => SingleChildScrollView(
+                    child: FollowupActions(
+                      caseLoad: widget.caseLoadModel,
+                    ),
+                  ),
+                );
               },
             ),
             const SizedBox(
